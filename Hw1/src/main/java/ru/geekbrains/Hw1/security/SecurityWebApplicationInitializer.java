fbec397d0072462461a2c6067cd8a236b7790ebd
chendyreva <nat-217@mail.ru>
2019-06-18 20:29:12 +0500

    Revert "1.1"
    This reverts commit bbeb3587ad2ec9c88a229c0505f1059ca85cf789.

--- Hw1/src/main/java/ru/geekbrains/Hw1/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.Hw1.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}

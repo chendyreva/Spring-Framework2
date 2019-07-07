348ca4655a41da67477a8b8b28e5a25991d47630
chendyrevanastya <40794468+chendyreva@users.noreply.github.com>
2019-07-07 15:14:58 +0500

    Revert "Hw 4"

--- Hw4/src/main/java/ru/geekbrains/ElectronicGeekMarket/persist/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.ElectronicGeekMarket.persist.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.geekbrains.ElectronicGeekMarket.persist.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- Hw4/src/main/java/ru/geekbrains/ElectronicGeekMarket/security/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.ElectronicGeekMarket.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
